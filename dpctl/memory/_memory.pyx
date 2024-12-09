ab56d6fc5dec7775b651694d67c765cd15f58994
Lukas Sommer <lukas.sommer@codeplay.com>
2024-12-09 10:22:02 +0000

    Add support for work_group_memory extension
    Extend kernel argument handling to add support for the
    work_group_memory extension, allowing users to dynamically allocate
    local memory for a kernel.
    
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

--- dpctl/memory/_memory.pyx
+++ dpctl/memory/_memory.pyx
@@ -893,6 +893,18 @@ cdef class MemoryUSMDevice(_Memory):
                         )
                     )
 
+cdef class WorkGroupMemory:
+    """
+    TODO: Add documentation.
+    """
+    def __cinit__(self, Py_ssize_t nbytes):
+        self.nbytes = nbytes
+
+    property nbytes:
+        """Extent of this USM buffer in bytes."""
+        def __get__(self):
+            return self.nbytes
+
 
 def as_usm_memory(obj):
     """
